1f8a887639d2e808f541f88d1368d4dff3343421
Home <Home@LAPTOP-QFF9CGK9>
2022-06-15 19:26:52 +0530

    First Commit

---
+++ SpringDependencyInjection/src/main/java/di1/Student1.java
@@ -0,0 +1,16 @@
+package di1;
+
+public class Student1 
+{
+	//Dependency in the form of objects
+			Cheat c;
+			public void setC(Cheat c) {
+				this.c = c;
+			}
+
+			public void cheating()
+			{
+				c.mathCheat();
+			}
+
+}
